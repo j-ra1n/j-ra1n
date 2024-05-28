578584794c0aa48adbd05ddd517bba83a0071df2
JoungWoo Lee <118893707+j-ra1n@users.noreply.github.com>
2024-05-28 21:19:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import feedparser
 
-def fetch_latest_blog_post():
+def fetch_latest_blog_posts(limit=3):
     feed = feedparser.parse("https://j-ra1n.tistory.com/rss")
-    latest_post = feed.entries[0]
-    title = latest_post.title
-    link = latest_post.link
-    return title, link
+    posts = []
+    for entry in feed.entries[:limit]:
+        title = entry.title
+        link = entry.link
+        posts.append((title, link))
+    return posts
 
-def update_readme(latest_post_title, latest_post_link):
+def update_readme(posts):
     with open("README.md", "r", encoding="utf-8") as file:
         readme_content = file.readlines()
 
     with open("README.md", "w", encoding="utf-8") as file:
         for line in readme_content:
-            if "<!-- LATEST_BLOG_POST -->" in line:
-                line = f"[{latest_post_title}]({latest_post_link})\n"
-            file.write(line)
+            if "<!-- LATEST_BLOG_POSTS -->" in line:
+                file.write("<!-- LATEST_BLOG_POSTS -->\n")
+                for title, link in posts:
+                    file.write(f"- [{title}]({link})\n")
+            else:
+                file.write(line)
 
 if __name__ == "__main__":
-    latest_post_title, latest_post_link = fetch_latest_blog_post()
-    update_readme(latest_post_title, latest_post_link)
- 
+    latest_posts = fetch_latest_blog_posts()
+    update_readme(latest_posts)
