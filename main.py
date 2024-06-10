961080e8725c51c1ce0c9b0de774a8e9b0ee570e
JoungWoo Lee <118893707+j-ra1n@users.noreply.github.com>
2024-06-10 20:51:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import feedparser
-import requests
-import time
 
 def fetch_latest_blog_posts(limit=3):
     feed = feedparser.parse("https://j-ra1n.tistory.com/rss")
@@ -8,33 +6,22 @@ def fetch_latest_blog_posts(limit=3):
     for entry in feed.entries[:limit]:
         title = entry.title
         link = entry.link
-        published = entry.published
-        posts.append((title, link, published))
+        posts.append((title, link))
     return posts
 
-def trigger_jenkins():
-    jenkins_url = 'http://localhost:8080/job/Jenkins_readme/build'
-    jenkins_user = '이정우'
-    jenkins_token = '115453658d957b826f819d28e0b0d27b59'
-    
-    response = requests.post(jenkins_url, auth=(jenkins_user, jenkins_token))
-    if response.status_code == 201:
-        print("Jenkins job triggered successfully")
-    else:
-        print(f"Failed to trigger Jenkins job: {response.status_code}")
+def update_readme(posts):
+    with open("README.md", "r", encoding="utf-8") as file:
+        readme_content = file.readlines()
 
-def main():
-    last_published = None
-    
-    while True:
-        latest_posts = fetch_latest_blog_posts()
-        if latest_posts:
-            title, link, published = latest_posts[0]
-            if last_published != published:
-                print(f"New post detected: {title}")
-                trigger_jenkins()
-                last_published = published
-        time.sleep(600)  # 10분마다 검사
+    with open("README.md", "w", encoding="utf-8") as file:
+        for line in readme_content:
+            if "<!-- LATEST_BLOG_POSTS -->" in line:
+                file.write("<!-- LATEST_BLOG_POSTS -->\n")
+                for title, link in posts:
+                    file.write(f'<a href="{link}" target="_blank">{title}</a><br>\n')
+            else:
+                file.write(line)
 
 if __name__ == "__main__":
-    main()
+    latest_posts = fetch_latest_blog_posts()
+    update_readme(latest_posts)
